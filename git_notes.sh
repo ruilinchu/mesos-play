3445b38a87c9755d3f8056f5799869cdc6c2dd36
ruilinchu <ruilinchu@gmail.com>
2015-11-30 14:43:04 -0800

    update

--- git_notes.sh
+++ git_notes.sh
@@ -8,17 +8,6 @@ cd roles
 git clone https://github.com/AnsibleShipyard/ansible-mesos
 git clone https://github.com/AnsibleShipyard/ansible-marathon
 
-cat >> ansible-mesos/defaults/main.yml <<EOF
 
-mesos_ip: "{{ ansible_enp0s8.ipv4.address }}" 
-mesos_version: "0.25.0" 
-
-EOF
-
-cat >>  ansible-marathon/defaults/main.yml <<EOF
-
-marathon_version: "0.11.1"
-
-EOF
 
 
