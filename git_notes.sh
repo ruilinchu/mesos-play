0cc6b7115e21b655ca8c5cef0b5fb0d0d5cfbeee
ruilinchu <ruilinchu@gmail.com>
2015-11-30 13:55:01 -0800

    update

--- git_notes.sh
+++ git_notes.sh
@@ -7,6 +7,18 @@ cd roles
 #git clone https://github.com/samos123/ansible-zookeeper
 git clone https://github.com/AnsibleShipyard/ansible-mesos
 git clone https://github.com/AnsibleShipyard/ansible-marathon
-echo mesos_ip: \"{{ ansible_enp0s8.ipv4.address }}\" >>  ansible-mesos/defaults/main.yml
+
+cat >> ansible-mesos/defaults/main.yml <<EOF
+
+mesos_ip: "{{ ansible_enp0s8.ipv4.address }}" 
+mesos_version: "0.25.0" 
+
+EOF
+
+cat >>  ansible-marathon/defaults/main.yml <<EOF
+
+marathon_version: "0.11.1"
+
+EOF
 
 
